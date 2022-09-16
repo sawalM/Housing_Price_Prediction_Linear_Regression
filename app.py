3110e2aabc719545b8e8101f6eca027e242c2ec4
sawalM <sawalmalhotra@gmail.com>
2022-09-16 13:34:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+#from django.shortcuts import render
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+#Creating object of Flask. Initstantitaing
+app=Flask(__name__)
+#__name__ indicates starting point for FFalskalsk execution/
+#Loading the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+#Creating Homepage using Flask 
+@app.route('/') 
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    #print(np.array(data.values().tolist()).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return str(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float (x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)
+    print(output)
+    return render_template('home.html',prediction_text='The House of prediction is {}'.format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
